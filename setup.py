fe00b4ae1d264a032c556c70343eed64ff2d08b1
timof121 <52547763+timof121@users.noreply.github.com>
2022-12-17 16:28:47 +0100

    fuck

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0'
+VERSION = '1.0.0'
 DESCRIPTION = 'Pretty print for Python'
 LONG_DESCRIPTION = 'Python package for having a prettier console output/input.'
 
