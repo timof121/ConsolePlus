56c64053f623c4e4f9b663778aa87c0797c0c936
timof121 <52547763+timof121@users.noreply.github.com>
2022-12-17 16:19:23 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0'
+DESCRIPTION = 'Pretty print for Python'
+LONG_DESCRIPTION = 'Python package for having a prettier console output/input.'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="consoleplus",
+    version=VERSION,
+    license="General Public License v3.0",
+    author="Evie",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'console', 'output', 'input', 'pretty', 'print']
+)
